b06869a22954a201e4121ce9f95766dd487955d6
Ra1nJun <Over4ction@gmail.com>
2025-11-18 20:58:34 +0900

    ci test

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import fs from 'fs';
 
+const isCI = process.env.CI === 'true';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync('../localhost+1-key.pem'),
-      cert: fs.readFileSync('../localhost+1.pem'),
-    },
-    port: 5173,
-  },
+  server: isCI ? {} : {
+      https: {
+        key: fs.readFileSync('../localhost+1-key.pem'),
+        cert: fs.readFileSync('../localhost+1.pem'),
+      },
+      port: 5173,
+    }
 })
